19b97034ec1ee840ec9ea92beb1563ffbdb8ec07
Jonathan Lim <jonathanlimsc92@gmail.com>
2015-07-18 22:53:03 +0800

    Added username modal box, username functionality and participant number tracking

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 /*var express = require('express');*/
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var usernames = []; //does not currently remove users upon disconnect.
+var numUsers = 0;
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -9,12 +11,19 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     console.log("Client connected");
+    numUsers++;
 
     socket.on('disconnect', function(){
         console.log("Client disconnected");
+        numUsers--;
     });
     socket.on('clientconnect', function(msg){
         console.log('Client Message: ' + msg);
+    });
+    socket.on('submitusername', function(username){
+        usernames.push(username);
+        io.emit('broadcastUserConnected', username);
+        io.emit('broadcastNumOfUsers', numUsers);
     });
     //Receiving chat message from client
     socket.on('chatmsg', function(msg){
